c353295c20ae3f30a9f44faa2a2e18a2fbafa83e
yashasvi <yashasvi.tiwari.4648@gmail.com>
2023-01-18 11:11:29 +0545

    Lab Report Submission By Yashasvi Tiwari

---
+++ student-submission/BESE2018/Yashasvi Tiwari/Interface.java
@@ -0,0 +1,19 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Interface extends Remote
+{
+    public void printMsg() throws RemoteException;
+    // method to calculate the area of circle 
+    public void calculateAreaOfCircle(double radius) throws RemoteException;
+
+    // calculate the tax amount 
+    public void calculateTax(double amount) throws RemoteException;
+
+    // set the tax rate 
+    public void setTaxRate(double taxRate) throws RemoteException;
+
+    // calculate the volume of rectangle
+
+    public void calculateVolumeOfRectangle(double length, double breadth, double height) throws RemoteException;
+}
