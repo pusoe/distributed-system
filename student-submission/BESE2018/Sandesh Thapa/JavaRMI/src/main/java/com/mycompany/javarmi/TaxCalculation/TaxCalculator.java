4f91821eddde3c9893d4ea78f134fdc8aebbc2e8
Sandesh Thapa Magar <66560689+Tsandesh@users.noreply.github.com>
2023-01-20 20:20:00 +0545

    Add files via upload
    Done Lab report 1 of RMI

---
+++ student-submission/BESE2018/Sandesh Thapa/JavaRMI/src/main/java/com/mycompany/javarmi/TaxCalculation/TaxCalculator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+  /* SANDESH THAPA */
+package com.mycompany.javarmi.TaxCalculation;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface TaxCalculator extends Remote {
+    double calculateTax(double amount) throws RemoteException;
+    void setTaxRate(double rate) throws RemoteException;
+}
