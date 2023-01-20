eb32369e5b77142e4a09190f5b7695955bf388be
binisha407 <binisha@skybase.com.np>
2023-01-20 12:00:33 +0545

    from bishal

---
+++ student-submission/BESE2018/bishal/Server.java
@@ -0,0 +1,33 @@
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server extends MyImplementation
+{
+    public Server()
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+        try {
+            // Instantiating the implementation class 
+            MyImplementation imp = new MyImplementation();
+    
+            // Exporting the object of implementation class  
+            // (here we are exporting the remote object to the stub) 
+            MyInterface stub = (MyInterface) UnicastRemoteObject.exportObject((Remote) imp, 8080);  
+         
+            // Binding the remote object (stub) in the registry 
+            Registry registry = LocateRegistry.getRegistry(); 
+         
+            registry.rebind("MyInterface", stub);  
+            System.err.println("Server ready"); 
+        } catch(Exception e) {
+            System.err.println("Server exception: " + e.toString()); 
+            e.printStackTrace(); 
+        }
+    }
+}
