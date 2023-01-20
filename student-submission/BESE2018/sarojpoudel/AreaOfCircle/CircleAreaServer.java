54faefd0d1fac42c2334e8392f7d16bcb2ef56bf
sarojluffy <Sarojreus10@gmail.com>
2023-01-20 23:05:54 +0545

    sarojpoudel

---
+++ student-submission/BESE2018/sarojpoudel/AreaOfCircle/CircleAreaServer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javarmi.AreaOfCircle;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class CircleAreaServer {
+  public static void main(String[] args) {
+    try {
+       Registry registry = LocateRegistry.createRegistry(5099);
+       registry.rebind("CircleArea", (Remote) new CircleAreaImpl());
+       System.out.println("server is ready");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
