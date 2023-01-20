ebd0aa6b35d4223e05ef9d12fb9a3fcd0a54ebd3
SurajaBaral <nanusuru07@gmail.com>
2023-01-20 14:05:05 +0545

    Lab report of Distributed system submmited by SurajaBaral

---
+++ student-submission/BESE2018/SurajaBaral/Client.java
@@ -0,0 +1,43 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.*;
+
+public class Client {
+    private Client() {
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        try {
+            // Getting the registry
+            Registry registry = LocateRegistry.getRegistry("localhost");
+
+            // Looking up the registry for the remote object
+            MyInterface stub = (MyInterface) registry.lookup("MyInterface");
+
+            // Calling the remote method using the obtained object
+            stub.printMsg();
+            stub.calculateAreaOfCircle(21.0);
+            stub.calculateVolumeOfRectangle(10.0, 12.0, 5.0);
+
+            try (// System.out.println("Remote method invoked");
+                    Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Enter the amount : ");
+                double amount = scanner.nextDouble();
+
+                System.out.println("Enter the tax rate: ");
+                double taxRate = scanner.nextDouble();
+
+                stub.setTaxRate(taxRate);
+                stub.calculateTax(amount);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
