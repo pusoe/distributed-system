4e627b76a9d59b8716d43e411151877736fdb12a
chi-rag <chiraggharti.m@gmail.com>
2023-01-21 16:41:56 +0545

    Chirag gharti 19180055 DS lab001

---
+++ student-submission/BESE2018/chirag/JAVARmi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javarmi;
+
+/**
+ *
+ * @author chirag
+ */
+public class JAVARmi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
